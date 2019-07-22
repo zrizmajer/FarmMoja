34c4a549813928ad9659e9da1d85888a18125bda
Zoltan Rizmajer <37550171+zrizmajer@users.noreply.github.com>
2019-07-22 20:55:41 +0100

    Added comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,19 @@
-jQuery(document).ready(function(){
-    $("a.scrollto").on("click", function(e) {
-        var target = this.hash;
-        console.log(target)
-        e.preventDefault();
+jQuery(document).ready(function() {
+  $("a.scrollto").on("click", function(e) {
+    var target = this.hash;
+    console.log(target);
+    e.preventDefault();
 
-        $('body').scrollTo(target, 800, {offset: -80, 'axis':'y', easing:'easeOutQuad'});
-        //Collapse mobile menu after clicking
-		if ($('.navbar-collapse').hasClass('show')){
-			$('.navbar-collapse').removeClass('show').addClass('collapse');
-		}
-    })
+    $("body").scrollTo(target, 800, {
+      offset: -80,
+      axis: "y",
+      easing: "easeOutQuad"
+    });
+    //Collapse mobile menu after clicking
+    if ($(".navbar-collapse").hasClass("show")) {
+      $(".navbar-collapse")
+        .removeClass("show")
+        .addClass("collapse");
+    }
+  });
 });
